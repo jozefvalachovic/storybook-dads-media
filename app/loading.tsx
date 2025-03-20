1cd71261e7a9c3a462bd6d02916eaee1995742db
jozefvalachovic <jozefvalachovic@gmail.com>
2025-03-20 07:43:55 +0100

    ADDS to 569d125e72d09502544fc08cb2fb6ffe1195341b

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,6 +1,7 @@
 export default function Loading() {
   return (
     <main>
+      <header></header>
       <section className="w-[var(--app-width-min)]">
         <div className="w-full flex flex-col gap-4 animate-pulse">
           <div className="h-8 bg-file-published rounded-2xl dark:bg-light-grey"></div>
